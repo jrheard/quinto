5c88de3730bfcce000205e26c5766dcc7353b456
jrheard <jrheard@cs.stanford.edu>
2017-11-24 14:32:49 -0800

    wip on reagent

--- src/quinto/core.cljs
+++ src/quinto/core.cljs
@@ -1,14 +1,30 @@
 (ns quinto.core
-    (:require [reagent.core :as r])
-    )
+  (:require [reagent.core :as r])
+  )
 
+(def BOARD-SIZE 10)
 
 (defonce app-state
-         (atom {:grid []}))
+         (atom {:grid (vec (repeat BOARD-SIZE (vec (repeat BOARD-SIZE nil))))}))
 
+(defn draw-cell [cell]
+  [:div.cell (if (nil? cell)
+               ""
+               cell)])
+
+(defn draw-row [row]
+  [:div.row
+   (for [index (range (count row))]
+     ^{:key index} [draw-cell (row index)])])
+
+(defn draw-grid [grid]
+  [:div#grid
+   (for [index (range (count grid))]
+     ^{:key index} [draw-row (grid index)])])
 
 (defn draw-game [state]
-  [:div "butt"]
+  [draw-grid (state :grid)]
+
   )
 
 (defn ^:export main []
@@ -16,5 +32,11 @@
                       (js/document.getElementById "app")))
 
 
+
 (defn on-js-reload []
   (main))
+
+(comment
+  (map-indexed identity (@app-state :grid))
+
+  )
