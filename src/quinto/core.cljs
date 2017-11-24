5f73b495942d5a5b544b621ae95c8d5567e9edd8
jrheard <jrheard@cs.stanford.edu>
2017-11-24 15:43:32 -0800

    wip on open-cell-finding logic

--- src/quinto/core.cljs
+++ src/quinto/core.cljs
@@ -1,46 +1,10 @@
 (ns quinto.core
   (:require [reagent.core :as r]
-            [quinto.html :refer [draw-game]]))
-
-; terminology to spec
-; a cell is an [x y] pair
-; a move is a [cell value] pair
-; a value is a number between 0 and 9 or nil
-; a cell is "empty" if its value is nil
-; a cell is "open" if it is possible for a player to make a move on that cell
-; a cell is "filled" if it has a non-nil value
-
-
-(def BOARD-SIZE 11)
-(def empty-grid (vec (repeat BOARD-SIZE (vec (repeat BOARD-SIZE nil)))))
+            [quinto.html :refer [draw-game]]
+            [quinto.grid :refer [empty-grid make-move find-open-cells]]))
 
 (defonce app-state
          (r/atom {:grid empty-grid}))
-
-(defn make-move [grid move]
-  ; a move is a list of [[x y] value] lists indicating where tiles should be placed.
-  (reduce (fn [grid [[x y] value]]
-            (assoc-in grid [x y] value))
-          grid
-          move))
-
-
-(defn find-empty-cells [grid]
-  ; return a list of [x y] values
-  (mapcat identity
-          (for [x (range (count grid))]
-            (for [y (range (count (grid x)))]
-              (when (nil? (get-in grid [x y]))
-                [x y])))))
-
-(defn cell-is-open? [grid cell]
-  ; TODO a cell is open if filling it will _not_ cause a streak of 6 or more filled cells to exist
-  )
-
-(defn find-open-cells [grid]
-  (filter cell-is-open? (find-empty-cells grid)))
-
-
 
 (defn ^:export main []
   (swap! app-state update-in [:grid] make-move [[[5 5] 0]
@@ -49,6 +13,7 @@
                                                 [[5 2] 5]
                                                 [[5 1] 5]])
 
+
   (r/render-component [draw-game @app-state]
                       (js/document.getElementById "app")))
 
@@ -56,6 +21,6 @@
   (main))
 
 (comment
-  (take 5 (find-empty-cells (@app-state :grid)))
+  (take 5 (find-open-cells (@app-state :grid)))
 
   )
---
+++ src/quinto/grid.cljs
@@ -0,0 +1,64 @@
+(ns quinto.grid)
+; terminology to spec
+; a cell is an [x y] pair
+; a move is a [cell value] pair
+; a value is a number between 0 and 9 or nil
+; a cell is "empty" if its value is nil
+; a cell is "open" if it is possible for a player to make a move on that cell
+; a cell is "filled" if it has a non-nil value
+; several "filled" cells in a row are a "run" of filled cells
+
+
+(def BOARD-SIZE 11)
+(def empty-grid (vec (repeat BOARD-SIZE (vec (repeat BOARD-SIZE nil)))))
+
+
+(defn make-move [grid move]
+  ; a move is a list of [[x y] value] lists indicating where tiles should be placed.
+  (reduce (fn [grid [[x y] value]]
+            (assoc-in grid [x y] value))
+          grid
+          move))
+
+
+(defn find-empty-cells [grid]
+  ; return a list of [x y] values
+  (mapcat identity
+          (for [x (range (count grid))]
+            (for [y (range (count (grid x)))]
+              (when (nil? (get-in grid [x y]))
+                [x y])))))
+
+(defn cell-is-off-grid [grid [x y]]
+  (and (>= x 0)
+       (< x (count grid))
+       (>= y 0)
+       (< y (count (first grid)))))
+
+
+(defn cell-is-open? [grid [x y]]
+  ; a cell is open if filling it will _not_ cause a streak of 6 or more filled cells to exist
+  (let [length-of-run-in-direction (fn [xdir ydir]
+                                     ; xxx there's gotta be a saner way to write this
+                                     (reduce (fn [cells-in-run steps-in-direction]
+                                               (let [run-x (+ x (* xdir steps-in-direction))
+                                                     run-y (+ y (* ydir steps-in-direction))]
+                                                 (if (or (cell-is-off-grid grid [run-x run-y])
+                                                         (nil? (get-in grid [run-x run-y])))
+                                                   (reduced cells-in-run)
+                                                   (inc cells-in-run))))
+                                             0
+                                             (range)))]
+
+    (cond (> (+ (length-of-run-in-direction 1 0)
+                (length-of-run-in-direction -1 0))
+             4)
+          false
+          (> (+ (length-of-run-in-direction 0 1)
+                (length-of-run-in-direction 0 -1))
+             4)
+          false
+          :else true)))
+
+(defn find-open-cells [grid]
+  (filter #(cell-is-open? grid %) (find-empty-cells grid)))
