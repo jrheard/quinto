b3cdedd3e129aad1004e39758d6ecd8563e04f1b
jrheard <jrheard@cs.stanford.edu>
2017-12-18 17:45:37 -0800

    when you place a tile, remove it from your hand

--- src/quinto/mode.cljs
+++ src/quinto/mode.cljs
@@ -1,5 +1,6 @@
 (ns quinto.mode
-  (:require [quinto.grid :as g]))
+  (:require [quinto.grid :as g]
+            [quinto.utils :refer [remove-item]]))
 
 (defn enter-assembling-move-mode [app-state selected-cell]
   (assert (contains? (set (g/find-playable-cells (app-state :grid)))
@@ -10,6 +11,7 @@
          {:mode/type       :assembling-move
           :selected-cell   selected-cell
           :available-cells []
+          :original-hand   (app-state :hand)
           :original-grid   (app-state :grid)}))
 
 (defn select-tile [app-state value]
@@ -17,6 +19,7 @@
     (-> app-state
         (assoc-in [:grid x y] value)
         (assoc-in [:mode :selected-cell] nil)
+        (update-in [:hand] remove-item value)
         ; xxxxx update available-cells
         ; this will be kind of tricky, because you have to handle situations where the move crosses one or more filled cells
         )))
@@ -24,4 +27,5 @@
 (defn cancel-mode [app-state]
   (cond-> app-state
     (contains? (app-state :mode) :original-grid) (assoc :grid (get-in app-state [:mode :original-grid]))
+    (contains? (app-state :mode) :original-hand) (assoc :hand (get-in app-state [:mode :original-hand]))
     true (assoc :mode {:mode/type :default})))
