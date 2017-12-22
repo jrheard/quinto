35d20dbb8fef39cc12730c082d3e1270d53b9a05
jrheard <jrheard@cs.stanford.edu>
2017-12-21 16:30:55 -0800

    tweak colors

--- src/quinto/specs.cljs
+++ src/quinto/specs.cljs
@@ -27,6 +27,3 @@
 
 (s/def :mode/type #{:default :assembling-move})
 (s/def ::mode (s/keys :req [:mode/type]))
-
-; TODO spec modes more, use a multispec
-; TODO spec app state, add an fdef in quinto.html that validates app state spec
